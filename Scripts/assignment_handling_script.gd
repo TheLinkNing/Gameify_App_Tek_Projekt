b152171a6e69cc35bcc56e000bcdcd043e252f39
TheLinkNing <diofon47@gmail.com>
2025-04-02 13:53:16 +0200

    Adds actual scripts

---
+++ Scripts/assignment_handling_script.gd
@@ -0,0 +1,10 @@
+extends Node
+
+class_name Assignments	
+
+func new(assignment_name, assignment_type, time_to_complete: int, assignment_difficulty):
+	
+	var assignment_info = [assignment_name, assignment_type, time_to_complete, assignment_difficulty]
+	return assignment_info
+
+func complete(assignment_info)
