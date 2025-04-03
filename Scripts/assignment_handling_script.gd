ec6ed7726cc04b34055496446560c31f7613343f
TheLinkNing <diofon47@gmail.com>
2025-04-03 13:19:34 +0200

    Update assignment_handling_script.gd

--- Scripts/assignment_handling_script.gd
+++ Scripts/assignment_handling_script.gd
@@ -4,7 +4,13 @@ class_name Assignments
 
 func new(assignment_name, assignment_type, time_to_complete: int, assignment_difficulty):
 	
-	var assignment_info = [assignment_name, assignment_type, time_to_complete, assignment_difficulty]
+	var assignment_info = {
+		"assignment_name": assignment_name,
+		"assignment_type": assignment_type,
+		"time_to_complete": time_to_complete,
+		"assignment_difficulty": assignment_difficulty
+	}
 	return assignment_info
 
-func complete(assignment_info, time_used, )
+func complete(assignment_info, time_used, ):
+	
