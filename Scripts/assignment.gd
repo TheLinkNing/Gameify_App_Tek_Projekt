175c8ecc43cfd2079ab71eb0eb0883b6b3c28f06
TheLinkNing <diofon47@gmail.com>
2025-04-22 12:04:27 +0200

    Update assignment.gd

--- Scripts/assignment.gd
+++ Scripts/assignment.gd
@@ -2,12 +2,13 @@ extends Node
 class_name	new_assignment
 
 # type of assignment, defined through a preditermined list of possible assignments
-@export var ass_type: String
+var ass_type: String
 # time in minutes
-@export var ass_time: int
+var ass_time: int
 # difficulty on a scale of easy to difficult
-@export var ass_diff: int
+var ass_diff: int
 
+# This function assigns values for the current assignment 
 func new_assignment(new_name, new_ass_type, new_ass_time, new_ass_diff):
 	
 	name = new_name
