ff4d0b7c9b6850f3e6944829e8b4c44f136700ef
TheLinkNing <diofon47@gmail.com>
2025-04-26 00:32:50 +0200

    Honestly I am so tired

--- appFiles/Scripts/assignment_manager.gd
+++ appFiles/Scripts/assignment_manager.gd
@@ -1,9 +1,13 @@
 extends Node
 
-const ASSIGNMENT: PackedScene = preload("res://App_Files/Scenes/assignment.tscn")
+const ASSIGNMENT: PackedScene = preload("res://appFiles/Scenes/assignment.tscn")
 
 func instantiate_new_assignment(ass_name, ass_type, ass_time, ass_diff):
+	# creates an instance of the assignment node as a child of the assignment_manager...
+	# using the variables obtained from the UI (aka. assignment_input)
 	var newr_assignment = ASSIGNMENT.instantiate()
 	add_child(newr_assignment)
 	newr_assignment.make_new_assignment(ass_name, ass_type, ass_time, ass_diff)
+	# here for debugging purposses
+	print(get_child_count())
 	
